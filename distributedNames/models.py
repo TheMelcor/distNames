fe9bd33676b73d1d5d1726f9ff74cfe2c1660477
TheMelcor <hakonme@online.no>
2017-03-15 15:54:32 +0100

    Added basic models

--- distributedNames/models.py
+++ distributedNames/models.py
@@ -6,3 +6,35 @@ from django.db import models
 
 # Make models if needed here!
 
+
+class Users:
+    name = ''
+
+    def __init__(self):
+        return
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+class Node:
+    ip = ""
+    isAuthNode = False
+
+    def __init__(self):
+        return
+
+    def set_ip(self, ip):
+        self.ip = ip
+
+    def get_ip(self):
+        return self.ip
+
+    def set_auth(self, auth):
+        self.isAuthNode = auth
+
+    def get_auth(self):
+        return self.isAuthNode
