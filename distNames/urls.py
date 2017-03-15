d4136794b757a451b70ef5ed9ba3000f20e96b17
TheMelcor <hakonme@online.no>
2017-03-15 16:50:26 +0100

    added form to index page

--- distNames/urls.py
+++ distNames/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('distributedNames.urls')),
+    url(r'^register/', include('distributedNames.urls')),
 ]
--- distributedNames/views.py
+++ distributedNames/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .forms import NameForm
 
 
 def index(request):
     return HttpResponse("hey guys")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/')
+    else:
+        form = NameForm()
+
+    return render(request, 'templates/register.html', {'form' : form})
